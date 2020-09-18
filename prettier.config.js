c8ec033df39053cb85b27d53fc232013c79ab02f
Josh Walters <josh.walters@outlook.com>
2020-09-18 02:40:35 +0000

    added debug launch config added prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  vueIndentScriptAndStyle: true,
+};
